88af278627912b2dd2c613b1b2c26f2edefc1f6d
Ginwoo Pak <ginwoopak@gmail.com>
2022-01-15 09:01:03 -0800

    still figuring out nestted arrays

--- server/index.js
+++ server/index.js
@@ -11,13 +11,34 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/products', (req, res) => {
   async function getAllProducts() {
     try {
-      const products = await sql`SELECT * FROM product LIMIT 5`;
+      const products = await sql`SELECT * FROM product LIMIT ${req.query.count}`;
       res.send(products);
     } catch(err) {
       console.log(err)
     }
   }
   getAllProducts()
+});
+
+app.get('/photos', (req, res) => {
+  async function getPhotos() {
+    try {
+      const photos = await sql`
+      SELECT
+        json_agg(json_build_object(
+          'url', photos.url,
+          'thumbnail_url', photos.thumbnail_url
+        )) as photos
+      FROM photos
+      GROUP BY photos.styleid
+      LIMIT 20
+      `;
+      res.send(photos);
+    } catch(err) {
+      console.log(err)
+    }
+  }
+  getPhotos()
 });
 
 app.get('/products/:product_id', (req, res) => {
@@ -39,23 +60,58 @@ app.get('/products/:product_id', (req, res) => {
 });
 
 app.get('/products/:product_id/styles', (req, res) => {
+  async function getPhotos() {
+    try {
+      const photos = await sql`
+      SELECT
+        json_agg(json_build_object(
+          'url', photos.url,
+          'thumbnail_url', photos.thumbnail_url
+        )) as photos
+      FROM photos
+      GROUP BY photos.styleid
+      LIMIT 20
+      `;
+      return photos;
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
   async function getStyles() {
     try {
-      // const styles = await sql`SELECT * FROM styles WHERE productId=1`;
       const styles = await sql`
-      SELECT features.product_id,
-      jsonb_agg(DISTINCT jsonb_build_object('style_id', styles.id, 'name', styles.name, 'original_price', styles.original_price, 'sale_price', styles.sale_price, 'default?', styles.default_style)) as results
+      SELECT
+        features.product_id,
+        jsonb_agg(
+          DISTINCT jsonb_build_object(
+            'style_id', styles.id,
+            'name', styles.name,
+            'original_price', styles.original_price,
+            'sale_price', styles.sale_price,
+            'default?', styles.default_style
+          )
+        ) as results
       FROM features
       LEFT JOIN styles ON styles.productId = features.product_id
       WHERE features.product_id = ${req.params.product_id}
       GROUP BY features.product_id
       `
-      res.send(styles);
+      console.log(styles.results)
+      return styles;
     } catch(err) {
       console.log(err);
     }
   }
-  getStyles()
+
+  getPhotos()
+  .then((photos) => {
+    getStyles()
+    .then((data) => {
+      console.log(data[0].results)
+      res.send(data[0].results)
+    })
+  })
 });
 
 
